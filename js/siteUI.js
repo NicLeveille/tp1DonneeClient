d50bd9dd34f92e5a8a8b04fe974654899a61df17
NicLeveille <nicleveille@hotmail.ca>
2023-09-18 11:03:44 -0400

    Client

---
+++ js/siteUI.js
@@ -0,0 +1,245 @@
+
+//<span class="cmdIcon fa-solid fa-ellipsis-vertical"></span>
+let contentScrollPosition = 0;
+Init_UI();
+
+function Init_UI() {
+    renderContacts();
+    $('#createContact').on("click", async function () {
+        saveContentScrollPosition();
+        renderCreateContactForm();
+    });
+    $('#abort').on("click", async function () {
+        renderContacts();
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+}
+
+function renderAbout() {
+    saveContentScrollPosition();
+    eraseContent();
+    $("#createContact").hide();
+    $("#abort").show();
+    $("#actionTitle").text("À propos...");
+    $("#content").append(
+        $(`
+            <div class="aboutContainer">
+                <h2>Gestionnaire de Bookmarks</h2>
+                <hr>
+                <p>
+                    Petite application de gestion de Bookmarks à titre de démonstration
+                    d'interface utilisateur monopage réactive.
+                </p>
+                <p>
+                    Auteur: Nicolas Léveillé
+                </p>
+                <p>
+                    Collège Lionel-Groulx, automne 2023
+                </p>
+            </div>
+        `))
+}
+async function renderContacts() {
+    showWaitingGif();
+    $("#actionTitle").text("Liste des Bookmarks");
+    $("#createContact").show();
+    $("#abort").hide();
+    let contacts = await API_GetBookmarks();
+    eraseContent();
+    if (contacts !== null) {
+        contacts.forEach(contact => {
+            $("#content").append(renderContact(contact));
+        });
+        restoreContentScrollPosition();
+        // Attached click events on command icons
+        $(".editCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderEditContactForm(parseInt($(this).attr("editContactId")));
+        });
+        $(".deleteCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderDeleteContactForm(parseInt($(this).attr("deleteContactId")));
+        });
+        $(".contactRow").on("click", function (e) { e.preventDefault(); })
+    } else {
+        renderError("Service introuvable");
+    }
+}
+function showWaitingGif() {
+    $("#content").empty();
+    $("#content").append($("<div class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+}
+function eraseContent() {
+    $("#content").empty();
+}
+function saveContentScrollPosition() {
+    contentScrollPosition = $("#content")[0].scrollTop;
+}
+function restoreContentScrollPosition() {
+    $("#content")[0].scrollTop = contentScrollPosition;
+}
+function renderError(message) {
+    eraseContent();
+    $("#content").append(
+        $(`
+            <div class="errorContainer">
+                ${message}
+            </div>
+        `)
+    );
+}
+function renderCreateContactForm() {
+    renderContactForm();
+}
+async function renderEditContactForm(id) {
+    showWaitingGif();
+    let contact = await API_GetBookmark(id);
+    if (contact !== null)
+        renderContactForm(contact);
+    else
+        renderError("Bookmark introuvable!");
+}
+async function renderDeleteContactForm(id) {
+    showWaitingGif();
+    $("#createContact").hide();
+    $("#abort").show();
+    $("#actionTitle").text("Retrait");
+    let contact = await API_GetBookmark(id);
+    eraseContent();
+    if (contact !== null) {
+        
+        $("#content").append(`
+        <div class="contactdeleteForm">
+            <h4>Effacer le Bookmark suivant?</h4>
+            <br>
+            <div class="contactRow" contact_id=${contact.Id}">
+                <div class="contactContainer">
+                    <div class="contactLayout">
+                        <div class="contactName">${contact.Name}</div>
+                        <div class="contactPhone">${contact.Logo}</div>
+                        <div class="contactEmail">${contact.Lien}</div>
+                    </div>
+                </div>  
+            </div>   
+            <br>
+            <input type="button" value="Effacer" id="deleteContact" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </div>    
+        `);
+        $('#deleteContact').on("click", async function () {
+            showWaitingGif();
+            let result = await API_DeleteBookmark(contact.Id);
+            if (result)
+                renderContacts();
+            else
+                renderError("Une erreur est survenue!");
+        });
+        $('#cancel').on("click", function () {
+            renderContacts();
+        });
+    } else {
+        renderError("Bookmark introuvable!");
+    }
+}
+function newContact() {
+    contact = {};
+    contact.Id = 0;
+    contact.Name = "";
+    contact.Lien = "";
+    contact.Logo = "";
+    return contact;
+}
+function renderContactForm(contact = null) {
+    $("#createContact").hide();
+    $("#abort").show();
+    eraseContent();
+    let create = contact == null;
+    if (create) contact = newContact();
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#content").append(`
+        <form class="form" id="contactForm">
+            <input type="hidden" name="Id" value="${contact.Id}"/>
+
+            <label for="Name" class="form-label">Nom </label>
+            <input 
+                class="form-control Alpha"
+                name="Name" 
+                id="Name" 
+                placeholder="Nom"
+                required
+                RequireMessage="Veuillez entrer un nom"
+                InvalidMessage="Le nom comporte un caractère illégal" 
+                value="${contact.Name}"
+            />
+            <label for="Phone" class="form-label">Catégorie </label>
+            <input
+                class="form-control"
+                name="Logo"
+                id="Phone"
+                placeholder="Catégorie"
+                required
+                RequireMessage="Veuillez entrer une catégorie" 
+                InvalidMessage="Veuillez entrer une catégorie valide"
+                value="${contact.Logo}" 
+            />
+            <label for="Email" class="form-label">Lien </label>
+            <input 
+                class="form-control "
+                name="Lien"
+                id="Email"
+                placeholder="Lien"
+                required
+                RequireMessage="Veuillez entrer votre lien" 
+                InvalidMessage="Veuillez entrer un lien valide"
+                value="${contact.Lien}"
+            />
+            <hr>
+            <input type="submit" value="Enregistrer" id="saveContact" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    initFormValidation();
+    $('#contactForm').on("submit", async function (event) {
+        event.preventDefault();
+        let contact = getFormData($("#contactForm"));
+        contact.Id = parseInt(contact.Id);
+        showWaitingGif();
+        let result = await API_SaveBookmark(contact, create);
+        if (result)
+            renderContacts();
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        renderContacts();
+    });
+}
+
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+
+function renderContact(contact) {
+    return $(`
+     <div class="contact" contact_id=${contact.Id}">
+        <div class="contactContainer ">
+            <div class="contactLayout">
+            <div class="big-favicon"
+                    style="background-image: url('http://www.google.com/s2/favicons?sz=64&domain=${contact.Lien}/');"></div>
+                <span class="contactEmail"><a href="${contact.Lien}">${contact.Name}</a> </span>
+            </div>
+            <div class="contactCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editContactId="${contact.Id}" title="Modifier ${contact.Name}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deleteContactId="${contact.Id}" title="Effacer ${contact.Name}"></span>
+            </div>
+        </div>
+    </div>           
+    `);
+}
